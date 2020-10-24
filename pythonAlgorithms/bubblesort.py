f4e1fd8de1eabdf6b375bfe6768108152c8ba95a
Ronin3790 <38832291+Ronin3790@users.noreply.github.com>
2020-10-24 06:11:14 +0430

    Update bubblesort.py

--- pythonAlgorithms/bubblesort.py
+++ pythonAlgorithms/bubblesort.py
@@ -1,10 +1,12 @@
-list = [25, 21, 22, 24, 23, 27, 26]
+def bubbleSort(list):
+    lastElementIndex = len(list)-1
+    for passNo in range(lastElementIndex,0,-1):
+        for idx in range(passNo):
+            if list[idx] > list[idx+1]:
+                list[idx], list[idx+1] = list[idx+1], list[idx]
+                return list
 
-lastElementIndex = len(list)-1
-print(0, list) 
-for idx in range(lastElementIndex):
-    if list[idx] > list[idx+1]:
-        list[idx], list[idx+1] = list[idx+1], list[idx]
-        print(idx+1, list) 
-    
+list = [25, 21, 22, 24, 23, 27, 26]
+print(bubbleSort(list))
+
     
